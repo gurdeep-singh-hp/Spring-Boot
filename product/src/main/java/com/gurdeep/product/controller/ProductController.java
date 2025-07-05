6a1698bc70757df7dc4336c4510d3b9dc00290f3
gurdeep-singh-hp <gurdeep.singh.hp@gmail.com>
2025-07-05 15:43:06 +0530

    Add/Find/FindAll/Delete/Update API for Cat n Pro

--- product/src/main/java/com/gurdeep/product/controller/ProductController.java
+++ product/src/main/java/com/gurdeep/product/controller/ProductController.java
@@ -1,19 +1,49 @@
 package com.gurdeep.product.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.gurdeep.product.dto.ProductDto;
+import com.gurdeep.product.service.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
+@AllArgsConstructor
 public class ProductController {
 
-    //Create
+    private ProductService productService;
 
-    //update by id
+    //Create Product
+    @PostMapping
+    public ResponseEntity<ProductDto> createProduct(@RequestBody ProductDto productDto){
+        return new ResponseEntity<>(productService.createProduct(productDto), HttpStatus.CREATED);
+    }
 
-    //Delete by id
+    //Get All Product
+    @GetMapping
+    public List<ProductDto> getAllProduct(){
+        return productService.getAllProduct();
+    }
 
-    //list All
+    //Get Product ById
+    @GetMapping
+    public ProductDto getProductById(@PathVariable Long id){
+        return productService.getProductById(id);
+    }
 
-    //Get One
+    //Delete Product ById
+    @DeleteMapping
+    public String deleteProduct(@PathVariable Long id){
+        return productService.deleteProduct(id);
+    }
+
+    //Update product ById
+    @PutMapping("/{id}")
+    public ProductDto updateProductById(@RequestBody ProductDto productDto, @PathVariable Long id){
+        return productService.updateProductById(productDto, id);
+    }
+
 }
--- product/src/main/java/com/gurdeep/product/service/ProductService.java
+++ product/src/main/java/com/gurdeep/product/service/ProductService.java
@@ -1,7 +1,69 @@
 package com.gurdeep.product.service;
 
+import com.gurdeep.product.dto.ProductDto;
+import com.gurdeep.product.entity.Category;
+import com.gurdeep.product.entity.Product;
+import com.gurdeep.product.mapper.ProductMapper;
+import com.gurdeep.product.repository.CategoryRepository;
+import com.gurdeep.product.repository.ProductRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@AllArgsConstructor
 public class ProductService {
+
+    private ProductRepository productRepository;
+    private CategoryRepository categoryRepository;
+
+    //Create Product
+    public ProductDto createProduct(ProductDto productDto){
+        //Check if categoryId Exist or Not
+        Category category = categoryRepository.findById(productDto.getCategoryId())
+                .orElseThrow(()-> new RuntimeException("Category not found!"));
+
+        Product product = ProductMapper.toProductEntity(productDto, category);
+        product = productRepository.save(product);
+        return ProductMapper.toProductDto(product);
+    }
+
+    //Get All Product
+    public List<ProductDto> getAllProduct(){
+        return productRepository.findAll().stream().map(ProductMapper::toProductDto).toList();
+    }
+    //Get Product ById
+    public ProductDto getProductById(Long id){
+        Product product = productRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Product not found for id: "+ id));
+        return ProductMapper.toProductDto(product);
+    }
+    //Delete Product ById
+    public String deleteProduct(Long id){
+        Product product = productRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Product not found for id: "+ id));
+        productRepository.deleteById(id);
+        return "Product delete successfully";
+    }
+    //Update product ById
+    public ProductDto updateProductById(ProductDto productDto, Long id){
+        Product product = productRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Product not found for id: "+ id));
+
+        //Need to check if Category changed and if they exist or not
+        Long cid = productDto.getCategoryId();
+        Category category = categoryRepository.findById(cid)
+                .orElseThrow(()-> new RuntimeException("Product not found for id: "+ id +" " +
+                        "and Category for id "+ cid));
+
+        product.setName(productDto.getName());
+        product.setDescription(productDto.getDescription());
+        product.setPrice(productDto.getPrice());
+        product.setCategory(category);
+
+        productRepository.save(product);
+        return ProductMapper.toProductDto(product);
+
+    }
 }
